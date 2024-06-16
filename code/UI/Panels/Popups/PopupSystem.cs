cf5af0a313035be133a7f30d731d165fa03a59af
trende2001 <68961063+trende2001@users.noreply.github.com>
2024-06-16 13:24:50 +0200

    Update PopupSystem.cs

--- code/UI/Panels/Popups/PopupSystem.cs
+++ code/UI/Panels/Popups/PopupSystem.cs
@@ -3,10 +3,10 @@ public partial class PopupSystem
 {
 	public static void DisplayPopup( string text, string title = "", float duration = 8f )
 	{
-		var Popup = new Popup();
-		Popup.Text = text;
-		Popup.Title = title;
-		Popup.Duration = duration;
-		PopupList.Instance.AddChild( Popup );
+		var popup = new Popup();
+		popup.Text = text;
+		popup.Title = title;
+		popup.Duration = duration;
+		PopupList.Instance.AddChild( popup );
 	}
 }
