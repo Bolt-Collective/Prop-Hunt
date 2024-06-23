b991ae3b678c116076053a808418b0a60521adf7
trende2001 <68961063+trende2001@users.noreply.github.com>
2024-06-23 15:23:13 -0400

    Add Particles and game object extensions

---
+++ code/Game/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,9 @@
+﻿
+public static class GameObjectExtensions
+{
+	public static void DestroyAsync( this GameObject self, float time )
+	{
+		var timedDestroy = self.Components.Create<TimeDestroy>();
+		timedDestroy.Time = time;
+	}
+}
---
+++ code/Game/Extensions/Particles.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+public static class Particles
+{
+	public static LegacyParticleSystem Create( string specParticle, Vector3 pos, Rotation rot, float decay = 5f )
+	{
+		var gameObject = Game.ActiveScene.CreateObject();
+		gameObject.Transform.Position = pos;
+		gameObject.Transform.Rotation = rot;
+
+		var particle = gameObject.Components.Create<LegacyParticleSystem>();
+		particle.Particles = ParticleSystem.Load( specParticle );
+		gameObject.Transform.ClearInterpolation();
+
+		Log.Info( "creating particle" );
+
+
+		// Clear off in a suitable amount of time.
+		gameObject.DestroyAsync( decay );
+
+		return particle;
+	}
+
+}
