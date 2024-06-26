bb651858d2a23b46b422cd6233946b929055058b
Nolankicks <nolandimensions@gmail.com>
2024-06-26 14:45:47 -0400

    Use custom map loader, replace doors with hinge joints, and replace unsupported lights with lights

--- code/Game/CustomMapLoader.cs
+++ code/Game/CustomMapLoader.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using Sandbox;
 
 public class CustomMapLoader : MapInstance
@@ -6,12 +7,37 @@ public class CustomMapLoader : MapInstance
 	{
 		if ( objectEntry.TypeName == "ent_door" )
 		{
-			var model = gameObject.Components.Create<ModelRenderer>();
-			model.Model = objectEntry.GetResource<Model>( "model" );
-			var collider = gameObject.Components.Create<ModelCollider>();
-			collider.Model = model.Model;
-			gameObject.Components.Create<Door>();
-			gameObject.NetworkSpawn( null );
+			//Create ModelRenderer
+			var renderer = gameObject.Components.Create<ModelRenderer>();
+			renderer.Model = objectEntry.GetResource<Model>( "model" );
+			//Create Rigidbody and st the locking and flags
+			var rb = gameObject.Components.Create<Rigidbody>();
+			var locking = new PhysicsLock();
+			locking.Z = true;
+			locking.X = true;
+			locking.Y = true;
+			locking.Pitch = true;
+			locking.Roll = true;
+			rb.Locking = locking;
+			rb.RigidbodyFlags = RigidbodyFlags.DisableCollisionSounds;
+			//Create ModelCollider and set the model
+			var modelCollider = gameObject.Components.Create<ModelCollider>();
+			modelCollider.Model = renderer.Model;
+			//Create HingeJoint, set the min and max angle, motor mode, and body.
+			var joint = gameObject.Components.Create<HingeJoint>();
+			joint.MinAngle = -80;
+			joint.MaxAngle = 80;
+			joint.Motor = HingeJoint.MotorMode.TargetAngle;
+		}
+		else if ( objectEntry.TypeName == "light_capsule" )
+		{
+			var light = gameObject.Components.Create<PointLight>();
+			light.LightColor = objectEntry.GetValue<Color>( "color" );
+		}
+		else if ( objectEntry.TypeName == "light_spot" )
+		{
+			var light = gameObject.Components.Create<SpotLight>();
+			light.LightColor = objectEntry.GetValue<Color>( "color" );
 		}
 	}
 }
