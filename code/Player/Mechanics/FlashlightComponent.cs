268c8418652eece191d07221c01b72704c4401d3
Nolankicks <nolandimensions@gmail.com>
2024-07-07 14:43:48 -0400

    Return if player is null

--- code/Player/Mechanics/FlashlightComponent.cs
+++ code/Player/Mechanics/FlashlightComponent.cs
@@ -6,33 +6,34 @@ public sealed class FlashlightComponent : Component
 
 
 	[Broadcast]
-	private void BroadcastFlashlight(Guid caller)
+	private void BroadcastFlashlight( Guid caller )
 	{
-		var player = Scene.Directory.FindByGuid(caller).Components.Get<Player>();
-		if (player == null) return;
+		var player = Scene.Directory.FindByGuid( caller ).Components.Get<Player>();
+		if ( player == null ) return;
 		Flashlight.Transform.Rotation = player.Eye.Transform.Rotation;
 		Flashlight.Transform.Position = player.Eye.Transform.Position + player.Eye.Transform.Rotation.Forward * 40;
 	}
 	[Broadcast]
-	private void EnableFlashlight(Guid caller)
+	private void EnableFlashlight( Guid caller )
 	{
-		var flashLight = Scene.Directory.FindByGuid(caller).Components.Get<FlashlightComponent>();
-		if (flashLight == null) return;
+		var flashLight = Scene.Directory.FindByGuid( caller ).Components.Get<FlashlightComponent>();
+		if ( flashLight == null ) return;
 
 		flashLight.Flashlight.Enabled = !flashLight.Flashlight.Enabled;
 	}
 	protected override void OnFixedUpdate()
 	{
-		if (Player.Local.TeamComponent.TeamName == Team.Unassigned.ToString()) return;
+		if ( Player.Local is null ) return;
+		if ( Player.Local.TeamComponent.TeamName == Team.Unassigned.ToString() ) return;
 
 		if ( Player.Local.TeamComponent.TeamName == Team.Props.ToString() ) return;
 
 		if ( IsProxy ) return;
 
-		BroadcastFlashlight(GameObject.Root.Id);
-		if (Input.Pressed("menu"))
+		BroadcastFlashlight( GameObject.Root.Id );
+		if ( Input.Pressed( "menu" ) )
 		{
-			EnableFlashlight(GameObject.Id);
+			EnableFlashlight( GameObject.Id );
 		}
 
 	}
