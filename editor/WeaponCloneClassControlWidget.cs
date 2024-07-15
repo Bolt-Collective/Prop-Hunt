4bc771ede41921864fe010415e31292b58ecd783
Nolankicks <nolandimensions@gmail.com>
2024-07-15 14:45:52 -0400

    Remove comment

--- editor/WeaponCloneClassControlWidget.cs
+++ editor/WeaponCloneClassControlWidget.cs
@@ -19,7 +19,6 @@ public class WeaponClassControlWidget : ControlWidget
 		var controlSheet = new ControlSheet();
 
 		var column = controlSheet.AddColumn();
-		//column.Margin = 5;
 		column.Add( new GameObjectControlWidget( serializedObject.GetProperty( "Weapon" ) ) );
 		column.AddSpacingCell( 5 );
 		column.Add( new VectorControlWidget( serializedObject.GetProperty( "Offset" ) ) );
