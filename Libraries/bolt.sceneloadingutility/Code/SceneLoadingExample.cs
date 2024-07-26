242be9796865c6ad31e3a1485fab111745f51a66
Nolankicks <nolandimensions@gmail.com>
2024-07-25 22:04:13 -0400

    More changes

--- Libraries/bolt.sceneloadingutility/Code/SceneLoadingExample.cs
+++ Libraries/bolt.sceneloadingutility/Code/SceneLoadingExample.cs
@@ -1,6 +1,30 @@
+using System.Linq;
+using Microsoft.VisualBasic;
 using Sandbox;
-
+using SceneLoading;
 public sealed class ChangeSceneTrigger : Component, Component.ITriggerListener
 {
+	[Property] public SceneFile sceneFile { get; set; }
+	[Property] public GameObject PrefabTest { get; set; }
 
+	void ITriggerListener.OnTriggerEnter( Sandbox.Collider other )
+	{
+		if ( other.GameObject.Tags.Has( "player" ) )
+		{
+			LoadScene();
+		}
+	}
+
+	void ITriggerListener.OnTriggerExit( Sandbox.Collider other )
+	{
+
+	}
+
+	public void LoadScene()
+	{
+		var customScene = new CustomScene( sceneFile );
+		var obj = customScene.GetAllObjectsByType( typeof( SkinnedModelRenderer ) ).FirstOrDefault();
+		customScene.CreateObject( PrefabTest );
+		customScene.LoadScene();
+	}
 }
--- Libraries/bolt.sceneloadingutility/Code/SceneLoadingUtility.cs
+++ Libraries/bolt.sceneloadingutility/Code/SceneLoadingUtility.cs
@@ -8,6 +8,7 @@ using System.Reflection.PortableExecutable;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
+using Microsoft.CSharp.RuntimeBinder;
 using Sandbox;
 using Sandbox.ActionGraphs;
 namespace SceneLoading
@@ -118,9 +119,12 @@ namespace SceneLoading
 
 	public class CustomScene
 	{
-		public SceneFile sceneFileDupe { get; set; }
+		[Description( "The scenefile you are manipulating, override to change it" )] public virtual SceneFile sceneFileDupe { get; set; }
 		public string RawScene { get; private set; }
 		public Scene newScene { get; private set; } = new();
+		[Description( "Called when a scene object is created, return the object you want to spawn, or null to use the default object" )]
+		public Action<JsonObject> OnSceneObjectCreated { get; set; }
+		public Action<JsonObject[]> BeforeSceneLoaded { get; set; }
 		public CustomScene( SceneFile sceneFile )
 		{
 			sceneFileDupe = new SceneFile();
@@ -129,65 +133,300 @@ namespace SceneLoading
 				.ToArray();
 		}
 
+		internal void LoadSceneInternal()
+		{
+			var finalScene = new SceneFile();
+			var finalList = new List<JsonObject>();
+			foreach ( var obj in sceneFileDupe.GameObjects )
+			{
+				OnSceneObjectCreated?.Invoke( obj );
+				finalList.Add( obj );
+			}
+
+			finalScene.GameObjects = finalList.ToArray();
+			BeforeSceneLoaded?.Invoke( finalScene.GameObjects );
+			Game.ActiveScene.Load( finalScene );
+		}
+		[Description( "Load the custom scene" )]
+		public void LoadScene()
+		{
+			LoadSceneInternal();
+		}
+
+		[Description( "Create a GameObject within the custom scene" )]
 		public void CreateObject( GameObject gameObject )
 		{
+			var clone = gameObject.Clone();
 			var objects = sceneFileDupe.GameObjects.ToList();
-			objects.Add( gameObject.Serialize() );
+			objects.Add( clone.Serialize() );
+			Log.Info( gameObject.Serialize().ToString() );
 			sceneFileDupe.GameObjects = objects.ToArray();
 		}
+
+		[Description( "Remove a GameObject from the custom scene" )]
 		public void RemoveObject( JsonNode obj )
 		{
 			List<JsonObject> gameObjects = sceneFileDupe.GameObjects.ToList();
-			Log.Info( obj );
 			var selectedObject = gameObjects.Find( x => x == obj );
-			Log.Info( sceneFileDupe.GameObjects.Length );
 			gameObjects.Remove( selectedObject );
-
 			sceneFileDupe.GameObjects = gameObjects.ToArray();
-			Log.Info( sceneFileDupe.GameObjects.Length );
 
 		}
 
-		public void LoadScene()
-		{
-			Game.ActiveScene.Load( sceneFileDupe );
-		}
 
-		public void RemoveComponent( JsonObject jsonObject, Type componentType )
+
+		public void RemoveComponentByType( JsonObject obj, Type type )
 		{
-			List<JsonObject> gameObjects = sceneFileDupe.GameObjects.ToList();
-			jsonObject.TryGetPropertyValue( "Components", out var jsonnode );
-			if ( jsonnode is not null )
+			var preObj = JsonSerializer.Deserialize<JsonObject>( JsonSerializer.Serialize( obj ) );
+			if ( obj.TryGetPropertyValue( "Components", out var jsonnode ) && jsonnode is not null )
 			{
 				var jsonString = jsonnode.ToString();
 				if ( !string.IsNullOrWhiteSpace( jsonString ) )
 				{
-					Log.Info( jsonString );
 					var components = JsonSerializer.Deserialize<List<JsonNode>>( jsonString );
-					components.RemoveAll( x => x["__type"]?.ToString() == componentType.ToString() );
-					jsonString = JsonSerializer.Serialize( components );
-					jsonObject["Components"] = jsonString;
+					var component = components.Find( x => x["__type"]?.ToString() == type.ToString() );
+					if ( component is not null )
+					{
+						components.Remove( component );
+						obj["Components"] = JsonSerializer.Deserialize<JsonNode>( JsonSerializer.Serialize( components ) );
+					}
 				}
 
+				var gbList = sceneFileDupe.GameObjects.ToList();
+				if ( gbList.Find( x => x == obj ) is null )
+				{
+					var parentNode = FindParent( preObj );
+					var parent = gbList.Find( x => x == parentNode );
+					if ( parent != null && parent.TryGetPropertyValue( "Children", out var childrenJsonNode ) && childrenJsonNode != null )
+					{
+						if ( childrenJsonNode is JsonArray childrenArray )
+						{
+							UpdateChildComponentsRecursively( childrenArray, obj );
+						}
+						else
+						{
+							Log.Error( "The 'Children' node is not of type 'JsonArray'." );
+						}
+					}
+				}
+				else
+				{
+					gbList[gbList.FindIndex( x => x == obj )] = obj;
+				}
+				sceneFileDupe.GameObjects = gbList.ToArray();
 			}
-			sceneFileDupe.GameObjects = gameObjects.ToArray();
 		}
+
+		private void UpdateChildComponentsRecursively( JsonArray childrenArray, JsonObject obj )
+		{
+			foreach ( var child in childrenArray )
+			{
+				if ( child is JsonObject childObject )
+				{
+					if ( childObject["__guid"].ToString() == obj["__guid"].ToString() )
+					{
+						childObject["Components"] = JsonSerializer.Deserialize<JsonNode>( JsonSerializer.Serialize( obj["Components"] ) );
+					}
+					else if ( childObject.TryGetPropertyValue( "Children", out var nestedChildrenJsonNode ) && nestedChildrenJsonNode is JsonArray nestedChildrenArray )
+					{
+						UpdateChildComponentsRecursively( nestedChildrenArray, obj );
+					}
+				}
+			}
+		}
+
+		public void AddComponent( JsonObject obj, JsonObject newComponent )
+		{
+			var preObj = JsonSerializer.Deserialize<JsonObject>( JsonSerializer.Serialize( obj ) );
+			if ( obj.TryGetPropertyValue( "Components", out var jsonnode ) && jsonnode is not null )
+			{
+				var jsonString = jsonnode.ToString();
+				if ( !string.IsNullOrWhiteSpace( jsonString ) )
+				{
+					var components = JsonSerializer.Deserialize<List<JsonNode>>( jsonString );
+					components.Add( newComponent );
+					obj["Components"] = JsonSerializer.Deserialize<JsonNode>( JsonSerializer.Serialize( components ) );
+				}
+				else
+				{
+					var components = new List<JsonNode> { newComponent };
+					obj["Components"] = JsonSerializer.Deserialize<JsonNode>( JsonSerializer.Serialize( components ) );
+				}
+
+				var gbList = sceneFileDupe.GameObjects.ToList();
+				if ( gbList.Find( x => x == obj ) is null )
+				{
+					var parentNode = FindParent( preObj );
+					var parent = gbList.Find( x => x == parentNode );
+					if ( parent != null && parent.TryGetPropertyValue( "Children", out var childrenJsonNode ) && childrenJsonNode != null )
+					{
+						if ( childrenJsonNode is JsonArray childrenArray )
+						{
+							UpdateChildComponentsRecursively( childrenArray, obj );
+						}
+						else
+						{
+							Log.Error( "The 'Children' node is not of type 'JsonArray'." );
+						}
+					}
+				}
+				else
+				{
+					gbList[gbList.FindIndex( x => x == obj )] = obj;
+				}
+				sceneFileDupe.GameObjects = gbList.ToArray();
+			}
+		}
+
+		public void AddComponentByType( JsonObject obj, Type componentType )
+		{
+			var preObj = JsonSerializer.Deserialize<JsonObject>( JsonSerializer.Serialize( obj ) );
+			if ( obj.TryGetPropertyValue( "Components", out var jsonnode ) && jsonnode is not null )
+			{
+				var jsonString = jsonnode.ToString();
+				var components = !string.IsNullOrWhiteSpace( jsonString )
+					? JsonSerializer.Deserialize<List<JsonNode>>( jsonString )
+					: new List<JsonNode>();
+
+				var newComponent = new JsonObject
+				{
+					["__type"] = componentType.ToString()
+				};
+
+				components.Add( newComponent );
+				obj["Components"] = JsonSerializer.Deserialize<JsonNode>( JsonSerializer.Serialize( components ) );
+
+				var gbList = sceneFileDupe.GameObjects.ToList();
+				if ( gbList.Find( x => x == obj ) is null )
+				{
+					var parentNode = FindParent( preObj );
+					var parent = gbList.Find( x => x == parentNode );
+					if ( parent != null && parent.TryGetPropertyValue( "Children", out var childrenJsonNode ) && childrenJsonNode != null )
+					{
+						if ( childrenJsonNode is JsonArray childrenArray )
+						{
+							UpdateChildComponentsRecursively( childrenArray, obj );
+						}
+						else
+						{
+							Log.Error( "The 'Children' node is not of type 'JsonArray'." );
+						}
+					}
+				}
+				else
+				{
+					gbList[gbList.FindIndex( x => x == obj )] = obj;
+				}
+				sceneFileDupe.GameObjects = gbList.ToArray();
+			}
+		}
+
+		public JsonObject FindParent( JsonNode children )
+		{
+			foreach ( var obj in sceneFileDupe.GameObjects )
+			{
+				var parent = FindParentRecursive( obj, children );
+				if ( parent != null )
+				{
+					return parent;
+				}
+			}
+			return null;
+		}
+
+		private JsonObject FindParentRecursive( JsonObject parent, JsonNode children )
+		{
+			if ( parent.TryGetPropertyValue( "Children", out var childrenJsonNode ) )
+			{
+				var childrenList = JsonSerializer.Deserialize<List<JsonObject>>( childrenJsonNode.ToString() );
+				if ( childrenList != null )
+				{
+					foreach ( var child in childrenList )
+					{
+						if ( child != null && child["__guid"].ToString() == children["__guid"].ToString() )
+						{
+							return parent;
+						}
+
+						var foundParent = FindParentRecursive( child, children );
+						if ( foundParent != null )
+						{
+							return parent;
+						}
+					}
+				}
+			}
+			return null;
+		}
+
 		public IEnumerable<JsonObject> GetAllObjectsByType( Type type )
 		{
-			return sceneFileDupe.GameObjects
-		  .Where( obj =>
-		  {
-			  if ( obj.TryGetPropertyValue( "Components", out var jsonnode ) && jsonnode != null )
-			  {
-				  var jsonString = jsonnode.ToString();
-				  if ( !string.IsNullOrWhiteSpace( jsonString ) )
-				  {
-					  var components = JsonSerializer.Deserialize<List<JsonNode>>( jsonString );
-					  return components.Any( component => component["__type"]?.ToString() == type.ToString() );
-				  }
-			  }
-			  return false;
-		  } );
+			return GetAllObjectsByTypeRecursive( sceneFileDupe.GameObjects, type );
+		}
+
+		public IEnumerable<JsonObject> GetAllObjectsByGuid( string guid )
+		{
+			return GetAllObjectsByGuidRecursive( sceneFileDupe.GameObjects, guid );
+		}
+
+		private IEnumerable<JsonObject> GetAllObjectsByTypeRecursive( IEnumerable<JsonObject> gameObjects, Type type )
+		{
+			foreach ( var obj in gameObjects )
+			{
+				if ( obj.TryGetPropertyValue( "Components", out var jsonnode ) && jsonnode is not null )
+				{
+					var jsonString = jsonnode.ToString();
+					if ( !string.IsNullOrWhiteSpace( jsonString ) )
+					{
+						var components = JsonSerializer.Deserialize<List<JsonNode>>( jsonString );
+						if ( components.Any( component => component["__type"]?.ToString() == type.ToString() ) )
+						{
+							yield return obj;
+						}
+					}
+				}
+
+				if ( obj.TryGetPropertyValue( "Children", out var childrenJsonNode ) && childrenJsonNode is not null )
+				{
+					var childrenString = childrenJsonNode.ToString();
+					if ( !string.IsNullOrWhiteSpace( childrenString ) )
+					{
+						var children = JsonSerializer.Deserialize<List<JsonObject>>( childrenString );
+						foreach ( var child in GetAllObjectsByTypeRecursive( children, type ) )
+						{
+							yield return child;
+						}
+					}
+				}
+			}
+		}
+
+		private IEnumerable<JsonObject> GetAllObjectsByGuidRecursive( IEnumerable<JsonObject> gameObjects, string guid )
+		{
+			foreach ( var obj in gameObjects )
+			{
+				if ( obj.TryGetPropertyValue( "__guid", out var jsonNode ) && jsonNode is not null )
+				{
+					var objectGuid = jsonNode.ToString();
+					if ( !string.IsNullOrWhiteSpace( objectGuid ) && objectGuid == guid )
+					{
+						yield return obj;
+					}
+				}
+
+				if ( obj.TryGetPropertyValue( "Children", out var childrenJsonNode ) && childrenJsonNode is not null )
+				{
+					var childrenString = childrenJsonNode.ToString();
+					if ( !string.IsNullOrWhiteSpace( childrenString ) )
+					{
+						var children = JsonSerializer.Deserialize<List<JsonObject>>( childrenString );
+						foreach ( var child in GetAllObjectsByGuidRecursive( children, guid ) )
+						{
+							yield return child;
+						}
+					}
+				}
+			}
 		}
 		public IEnumerable<JsonNode> GetAllObjectsByName( string name )
 		{
