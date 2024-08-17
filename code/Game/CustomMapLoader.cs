42d016f88a0e2283fc873ac2b91581f102dda1b2
trende2001 <68961063+trende2001@users.noreply.github.com>
2024-08-17 13:04:13 +0200

    Support legacy doors in source engine maps

--- code/Game/CustomMapLoader.cs
+++ code/Game/CustomMapLoader.cs
@@ -5,27 +5,35 @@ public class CustomMapLoader : MapInstance
 {
 	protected override void OnCreateObject( GameObject gameObject, MapLoader.ObjectEntry objectEntry )
 	{
-		/*if ( objectEntry.TypeName == "ent_door" )
+		if ( objectEntry.TypeName == "ent_door" )
 		{
-			//Create ModelRenderer
-			var renderer = gameObject.Components.Create<ModelRenderer>();
-			renderer.Model = objectEntry.GetResource<Model>( "model" );
-			//Create Rigidbody and st the locking and flags
-			var rb = gameObject.Components.Create<Rigidbody>();
-			var locking = new PhysicsLock();
-			locking.Pitch = true;
-			locking.Roll = true;
-			rb.Locking = locking;
-			rb.RigidbodyFlags = RigidbodyFlags.DisableCollisionSounds;
-			//Create ModelCollider and set the model
-			var modelCollider = gameObject.Components.Create<ModelCollider>();
-			modelCollider.Model = renderer.Model;
-			//Create HingeJoint, set the min and max angle, motor mode, and body.
-			var joint = gameObject.Components.Create<HingeJoint>();
-			joint.MinAngle = -80;
-			joint.MaxAngle = 80;
-			joint.Motor = HingeJoint.MotorMode.TargetAngle;
-		}*/
+			Model resource = objectEntry.GetResource<Model>( "model" );
+
+			var skMdl = gameObject.Components.Create<SkinnedModelRenderer>();
+			skMdl.Model = resource;
+			var mdlCollider = gameObject.Components.Create<ModelCollider>();
+			mdlCollider.Model = resource;
+
+			var door = gameObject.Components.Create<Door>();
+
+			Angles movedir = objectEntry.GetValue<Angles>( "movedir" );
+			float distance = objectEntry.GetValue<float>( "distance" );
+			Vector3 origin = objectEntry.GetValue<Vector3>( "pivot" );
+
+			bool startslocked = objectEntry.GetValue<bool>( "startslocked" );
+			bool locked = objectEntry.GetValue<bool>( "locked" );
+
+			Door.DoorMoveType movedir_type = objectEntry.GetValue<Door.DoorMoveType>( "movedir_type" );
+			door.Axis = movedir;
+			door.Distance = distance;
+			door.MoveDirType = movedir_type;
+			door.Locked = locked || startslocked;
+			door.PivotPosition = gameObject.Transform.World.PointToWorld( origin );
+			door.Collider = mdlCollider;
+
+			gameObject.SetParent( GameObject );
+			gameObject.NetworkSpawn( null );
+		}
 
 	}
 }
